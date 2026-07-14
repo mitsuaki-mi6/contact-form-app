aac93952982fbe6eaec169e29523ff2f713f3e04
mitsuaki <mi6chigasaki@gmail.com>
2026-07-14 12:39:36 +0900

    feat: Fortifyを使った管理者ログイン機能の実装

---
+++ lang/ja/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'required' => ':attributeを入力してください',
+    'string' => ':attributeは文字列で入力してください。',
+    'max' => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+    'attributes' => [
+        'title' => 'タイトル',
+        'description' => '説明',
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+    ],
+];
